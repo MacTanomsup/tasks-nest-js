6842ab2a95a22b546ec33c4ea47c67a3e11aef20
MacTanomsup <mactanomsup@gmail.com>
2022-01-10 08:58:45 +0700

    Add passport and implement auth by using jwt strategy

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,36 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+
+    // hash
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    console.log('salt', salt);
+    console.log('hashedPassword', hashedPassword);
+
+    const user = this.create({ username, password: hashedPassword });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      console.log(error);
+      if (error.code === '23505') {
+        // duplicate username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
